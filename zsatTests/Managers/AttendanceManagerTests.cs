aefedd251248b09da7640ec7c273aedd85afae31
Digna Bringule <dign0009@edu.easj.dk>
2022-12-05 14:43:07 +0100

    More tests Add - test improvements

--- zsatTests/Managers/AttendanceManagerTests.cs
+++ zsatTests/Managers/AttendanceManagerTests.cs
@@ -8,54 +8,69 @@ using System.Threading.Tasks;
 using zsat.Models;
 using zsat.Interfaces;
 using NuGet.Packaging.Signing;
+using Microsoft.EntityFrameworkCore;
 
 namespace zsat.Managers.Tests
 {
     [TestClass]
     public class AttendanceManagerTests
     {
-        private readonly AttendanceManager _manager;
-        public AttendanceManagerTests() { }
-        public AttendanceManagerTests(AttendanceManager manager)
+        string _connectionString = "Data Source=tcp:zsatdb.database.windows.net,1433;Initial Catalog=zsattestdb;User Id=zsatadmin@zsatdb;Password=Zealand123$";
+        ZsatDbContext _context;
+        AttendanceManager _manager;
+
+        public AttendanceManagerTests()
         {
-            _manager = manager;
+            var options = new DbContextOptionsBuilder<ZsatDbContext>().UseSqlServer(_connectionString).Options;
+            _context = new ZsatDbContext(options);
+            _manager = new AttendanceManager(_context);
         }
 
-        [TestMethod()]
+        AppUser user1 = new AppUser() { Name = "Digna", Email = "dign0009@edu.zealand.dk" };
+        CardUser cardUser1 = new CardUser() { CardId = "1" , AppUserId = "33f88254-954b-4633-a80c-8cc0fca880fd" };
+        Lesson lesson1 = new Lesson() { Id = 1, Subject = "Math" };
+
+        List<Attendance> attendances = new List<Attendance>();
+
+        //public void Initialize()
+        //{
+        //    _context.AppUsers.Add(user1);
+        //    _context.CardUsers.Add(cardUser1);
+        //    _context.Lessons.Add(lesson1);
+        //    _context.SaveChanges();
+        //}
+
+        [TestMethod]
         public void GetEmptyAttendancesTest()
-        {
-            //arrange
-            _manager.RegisterAttendance("1", DateTime.Now);
-            List<Attendance> attendances = new List<Attendance>();
-            //act
+        { 
             attendances = _manager.GetAllAttendances().Result;
-            //assert
-            Assert.AreEqual(1, attendances.Count);
+
+            Assert.AreEqual(0, attendances.Count);
         }
 
         [TestMethod]
         public void GetAllAttendancesTest()
         {
-            //arrange
-            List<Attendance> attendances = new List<Attendance>();
-            //act
+            Attendance attendance = new Attendance() { CardUserId = cardUser1.CardId, LessonId = 1 };
+            _context.Attendances.Add(attendance);
+
             attendances = _manager.GetAllAttendances().Result;
-            //assert
-            Assert.AreEqual(0, attendances.Count);        
+
+            Assert.AreEqual(1, attendances.Count);
         }
 
         [TestMethod]
         public void RegisterAttendanceTest()
         {
-            //arrange
-            string cardId = "2";
+            string cardId = "1";
             DateTime timestamp = DateTime.Now;
-            //act
+
             Attendance attendance = _manager.RegisterAttendance(cardId, timestamp).Result;
             List<Attendance> attendances = _manager.GetAllAttendances().Result;
-            //assert
+
             Assert.AreEqual(2, attendances.Count);
         }
+
         [TestMethod]
         public void RegisterInvalidAttendanceTest()
         {
